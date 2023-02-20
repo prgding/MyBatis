8d0506b49e1d71e959f2809a0982d8f89e2de1d4
Ding Shuai <1203823603@qq.com>
2023-02-20 08:14:13 +0800

    map 集合传参

---
+++ src/test/java/CarMapperTest.java
@@ -0,0 +1,52 @@
+import me.ding.utils.SqlSessionUtil;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+public class CarMapperTest {
+	@Test
+	public void testInsert() {
+		SqlSession sqlSession = null;
+		try {
+			SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
+			SqlSessionFactory sqlSessionFactory = sqlSessionFactoryBuilder.build(Resources.getResourceAsStream("mybatis-config.xml"));
+			sqlSession = sqlSessionFactory.openSession();
+			int count = sqlSession.insert("insertCar");
+			System.out.println(count);
+			sqlSession.commit();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (sqlSession != null) {
+				sqlSession.close();
+			}
+		}
+	}
+
+	@Test
+	public void testUtil() {
+		SqlSession sqlSession = SqlSessionUtil.openSession();
+		sqlSession.insert("insertCar");
+		sqlSession.commit();
+		sqlSession.close();
+	}
+
+	@Test
+	public void testMap() {
+		HashMap<String, Object> map = new HashMap<>();
+		map.put("carNum", "111");
+		map.put("brand", "Porsche");
+		map.put("producePrice", 100);
+		map.put("produceTime", "2022-02-15");
+		map.put("carType", "燃油车");
+		SqlSession sqlSession = SqlSessionUtil.openSession();
+		sqlSession.insert("insertCar", map);
+		sqlSession.commit();
+		sqlSession.close();
+	}
+}
